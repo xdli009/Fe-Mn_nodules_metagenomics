4fb4a0e4397dd7e098079d0423a8fffc2ba122a8
xdli <46087474+xdli009@users.noreply.github.com>
2023-03-04 19:14:53 +0800

    Delete 1_metagenomics_assembly directory

--- 1_metagenomics_assembly/prodigal.sh
+++
@@ -1,4 +0,0 @@
-for name in A5-S03 A5-S04 KW1-S05 KW1-S08 KW1-S11 KW1-S15 KW1-S21
-do
-prodigal -i ./pullseq_result/${name}_1000bp.fasta -f gff -o ./prodigal_result/${name}.gff -a ./prodigal_result/${name}.faa -d ./prodigal_result/${name}.ffn -p meta
-done
