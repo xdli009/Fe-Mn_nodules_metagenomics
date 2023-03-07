a3aadbf864d0f8e389871b61ad8677627377a387
xdli <46087474+xdli009@users.noreply.github.com>
2023-03-07 21:04:06 +0800

    Delete 5_MAG_abundance/fig1_visualization directory

--- 5_MAG_abundance/fig1_visualization/gtdb_tree.sh
+++
@@ -1,2 +0,0 @@
-iqtree -s ../../4_MAG_dereplication/gtdb_result/gtdbtk.ar122.user_msa.fasta -m TEST -alrt 1000 -bb 1000 -nt 72
-iqtree -s ../../4_MAG_dereplication/gtdb_result/gtdbtk.bac120.user_msa.fasta -m TEST -alrt 1000 -bb 1000 -nt 72
