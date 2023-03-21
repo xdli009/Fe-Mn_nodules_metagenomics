1b60b2fce53586c90f58c93ede989197b6826dc6
xdli <46087474+xdli009@users.noreply.github.com>
2023-03-21 19:46:40 +0800

    Add files via upload

---
+++ 5_MAG_abundance/fig1_visualization/gtdb_tree.sh
@@ -0,0 +1,2 @@
+iqtree -s ../../4_MAG_dereplication/gtdb_result/gtdbtk.ar122.user_msa.fasta -m TEST -alrt 1000 -bb 1000 -nt 72
+iqtree -s ../../4_MAG_dereplication/gtdb_result/gtdbtk.bac120.user_msa.fasta -m TEST -alrt 1000 -bb 1000 -nt 72
